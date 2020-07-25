170f7f34953ae94adbba0663f2aa37b4322e27d9
Ahmed-Adel-2040 <ahmed.adel.abosood@gmail.com>
2020-07-25 14:50:40 +0200

    get the mistake in the text

--- correct.py
+++ correct.py
@@ -1,5 +1,7 @@
 import language_check
 lang_tool = language_check.LanguageTool("en-US")
-text = "A sentence with a error in the Hitchhiker’s Guide tot he Galaxy"
+text = "A sentence with a error in the Hitchhiker’s Guide tot he Galaxy. Ahmed Adel are big enginee And data analysite"
 matches = lang_tool.check(text)
-print(matches)
+print('Number of mistakes',len(matches))
+for mistake in matches:
+    print(mistake)

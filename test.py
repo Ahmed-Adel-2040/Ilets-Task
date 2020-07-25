0dd38f76487862e3d4a2062eb9c7dcc251cc00b8
Ahmed-Adel-2040 <ahmed.adel.abosood@gmail.com>
2020-07-25 16:03:58 +0200

    add new Action

--- test.py
+++ test.py
@@ -1,23 +1,42 @@
 import sys
-
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import  QApplication,QFrame
 from PyQt5.uic import loadUi
+import language_check
+
+
+lang_tool = language_check.LanguageTool("en-US")
+global Grammer_Score
+global Lexis_Score
 
 @pyqtSlot()
 class Ilets(QFrame) :
+
+    #MORFOLOGIK_RULE_EN_US
+    Grammer_Score=0
+    Lexis_Score=0
+
     def __init__(self):
         super(Ilets,self).__init__()
         loadUi("main.ui",self)
         self.setWindowTitle("ILETS Correct")
-        #intro_correct_button
         self.intro_correct_button.clicked.connect(self.actionButton)
 
     def actionButton(self):
-        self.intr_lexis_lable.setText("Ok Dev")
-        self.intro_grammer_lable.setText("Ok Gogo"+self.intro_textEdit.toPlainText ())
-        t=self.intro_textEdit.toPlainText ()
-        self.intro_textEdit.setText("Ok Gogo"+t)
+        text = self.intro_textEdit.toPlainText()
+        self.grammerScore(text)
+        self.intro_grammer_lable.setText(Grammer_Score)
+        self.intr_lexis_lable.setText(Lexis_Score)
+
+
+    def grammerScore(Text=''):
+        Mistakes = lang_tool.check(Text)
+        for mistake in Mistakes:
+            if mistake.ruleId != 'MORFOLOGIK_RULE_EN_US':
+                Grammer_Score += 1
+            else:
+                Lexis_Score += 1
+
 
     
 app=QApplication(sys.argv)

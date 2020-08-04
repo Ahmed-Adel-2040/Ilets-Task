ed3bce1bc0d01d71706e79db307177dbfc1b3fd3
Ahmed-Adel-2040 <ahmed.adel.abosood@gmail.com>
2020-08-04 16:33:59 +0200

    font added

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import re
 import sys
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QFont
 from PyQt5.QtWidgets import QApplication,QFrame
 from PyQt5.uic import loadUi
 import language_check
@@ -10,6 +10,10 @@ import language_check
 @pyqtSlot()
 class Ilets(QFrame):
     #MORFOLOGIK_RULE_EN_US
+    first_List=[]
+    second_List=[]
+    opinion_List=[]
+    intro_List=[]
     def __init__(self):
         super(Ilets,self).__init__()
         loadUi("main.ui",self)
@@ -18,17 +22,19 @@ class Ilets(QFrame):
         self.first_correct_button.clicked.connect(self.first_actionButton)
         self.second_correct_button.clicked.connect(self.second_actionButton)
         self.opinion_correct_button.clicked.connect(self.opinion_actionButton)
+        self.clear_button.clicked.connect(self.clear_actionButton)
 
     def actionButton(self):
         text = self.intro_textEdit.toPlainText()
         Quest= self.Quest_textEdit.toPlainText()
         #print(self.intro_textEdit.setText(Back.LIGHTBLACK_EX+text))
-        G_Score,L_Score=self.get_Score(text)
+        G_Score,L_Score,self.intro_List=self.get_Score(text)
         colorText=self.change_Color(text,Quest)
         self.intro_textEdit.clear()
         self.intro_textEdit.append(colorText)
         self.intro_grammer_lable.setText(G_Score)
-        self.intr_lexis_lable.setText(L_Score)
+        self.intro_lexis_lable.setText(L_Score)
+        self.intro_lexis_lable.setFont(QFont("Arial", 11, QFont.Bold))
 
 
     def get_Score(self,text):
@@ -36,12 +42,19 @@ class Ilets(QFrame):
         Lexis_Score = 0
         lang_tool = language_check.LanguageTool("en-US")
         Mistakes = lang_tool.check(text)
+        IndexList=[]
         for mistake in Mistakes:
+            smallList=[]
+            mistakeList=list(mistake)
+            smallList.append(mistakeList[1])
+            smallList.append(mistakeList[3])
+            IndexList.append((smallList))
+
             if mistake.ruleId != 'MORFOLOGIK_RULE_EN_US':
                 Grammer_Score += 1
             else:
                 Lexis_Score += 1
-        return  str(Grammer_Score),str(Lexis_Score)
+        return  str(Grammer_Score),str(Lexis_Score),IndexList
 
     def spiltParagraph(self,text):
         sent = re.split(r' *[\.\?!][\'"\)\]]* *', text)
@@ -49,19 +62,19 @@ class Ilets(QFrame):
 
     def first_actionButton(self):
         text=self.first_textEdit.toPlainText()
-        G_Score,L_Score=self.get_Score(text)
+        G_Score,L_Score,self.first_List=self.get_Score(text)
         self.first_grammer_lable.setText(G_Score)
         self.first_lexis_lable.setText(L_Score)
 
     def second_actionButton(self):
         text = self.second_textEdit.toPlainText()
-        G_Score,L_Score=self.get_Score(text)
+        G_Score,L_Score,self.second_List=self.get_Score(text)
         self.second_grammer_lable.setText(G_Score)
-        self.scecond_lexis_lable.setText(L_Score)
+        self.second_lexis_lable.setText(L_Score)
 
     def opinion_actionButton(self):
         text = self.opinion_textEdit.toPlainText()
-        G_Score,L_Score=self.get_Score(text)
+        G_Score,L_Score,self.opinion_List=self.get_Score(text)
         self.opinion_grammer_lable.setText(G_Score)
         self.opinion_lexis_lable.setText(L_Score)
 
@@ -83,7 +96,7 @@ class Ilets(QFrame):
                 redText += "</span>"
             else:
                 simi = .2
-                redText += "<span style=\" font-size:15pt; font-weight:600; background-color:#ffe1f0;\" >"
+                redText += "<span style=\" font-size:15pt; font-weight:600; background-color:#C0C0C0;\" >"
                 redText += sent
                 redText += "</span>"
         return redText
@@ -94,6 +107,19 @@ class Ilets(QFrame):
 
 
 
+    def clear_actionButton(self):
+        self.intro_textEdit.clear()
+        self.first_textEdit.clear()
+        self.second_textEdit.clear()
+        self.opinion_textEdit.clear()
+        self.intro_grammer_lable.setText('Score')
+        self.intro_lexis_lable.setText('Score')
+        self.first_grammer_lable.setText('Score')
+        self.second_grammer_lable.setText('Score')
+        self.opinion_grammer_lable.setText('Score')
+        self.first_lexis_lable.setText('Score')
+        self.second_lexis_lable.setText('Score')
+        self.opinion_lexis_lable.setText('Score')
 
 
 
